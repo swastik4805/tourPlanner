22ead9204104cd0bca73e4be549a26507055567e
chhono <chatterjeeswastik7772@gmail.com>
2025-01-06 22:47:37 +0530

    fixex #4

--- backend/main.go
+++ backend/main.go
@@ -16,16 +16,24 @@ import (
 )
 
 type Tour struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name      string             `bson:"name" json:"name"`
-	StartDate string             `bson:"startdate" json:"startdate"`
-	CreatedAt time.Time          `bson:"createdat" json:"createdat"`
+	ID        primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
+	Name      string               `bson:"name" json:"name"`
+	StartDate string               `bson:"startdate" json:"startdate"`
+	CreatedAt time.Time            `bson:"createdat" json:"createdat"`
+	userIDs   []primitive.ObjectID `bson:"tour_ids" json:"tour_ids"`
+}
+
+type User struct {
+	ID      primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
+	Name    string               `bson:"name" json:"name"`
+	Email   string               `bson:"email" json:"email"`
+	TourIDs []primitive.ObjectID `bson:"user_ids" json:"user_ids"`
 }
 
 var dbName = "tourPlanner"
-
 var tourCollection *mongo.Collection
 var deletedCollection *mongo.Collection
+var userCollection *mongo.Collection
 
 func init() {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
@@ -37,6 +45,7 @@ func init() {
 	println("Mongo db connected")
 	tourCollection = client.Database(dbName).Collection("tours")
 	deletedCollection = client.Database(dbName).Collection("deletedTours")
+	userCollection = client.Database(dbName).Collection("users")
 	println("collection instance in ready")
 }
 
@@ -91,10 +100,38 @@ func deleteTour(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("inserted one tour", inserted.InsertedID)
 }
 
+func AddUserToRoute(w http.ResponseWriter, r *http.Request) {
+	//get the tour_id from the url
+	params := mux.Vars(r)
+	tour_id, _ := primitive.ObjectIDFromHex(params["tour_id"])
+
+	//get the id of the user from the body
+	var userID primitive.ObjectID
+	err := json.NewDecoder(r.Body).Decode(&userID)
+	if err != nil {
+		fmt.Println("some problem with the request data")
+		return
+	}
+
+	//find the tour with the tour_id
+	filter := bson.M{"_id": tour_id}
+	update := bson.M{"$addToSet": bson.M{"user_ids": userID}}
+	updated, err := tourCollection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		fmt.Println("some error occured while adding in db")
+		return
+	}
+	fmt.Println("added user to tour", updated)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(bson.M{"message": "user added successfully"})
+
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/createTour", CreateTour).Methods("POST")
 	r.HandleFunc("/updateTour/{id}", UpdateTour).Methods("PUT")
 	r.HandleFunc("/deleteTour/{id}", deleteTour).Methods("DELETE")
+	r.HandleFunc("/addUserToTour/{tour_id}", AddUserToRoute).Methods("POST")
 	http.ListenAndServe(":8080", r)
 }
